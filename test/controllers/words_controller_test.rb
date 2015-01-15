aa1b2c1c2435842def5d0a2c52a706d7d74a4300
Fabian Schneider <fschneider.mmt-m2014@fh-salzburg.ac.at>
2015-01-15 21:58:18 +0000

    fixed Word tests

--- test/controllers/words_controller_test.rb
+++ test/controllers/words_controller_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class WordsControllerTest < ActionController::TestCase
   
+  def setup
+    @lang = Language.create(name: 'Testlang', code: 'TL')
+  end
+  
   test "should get index" do
     get :index
     assert_response :success
@@ -10,7 +14,7 @@ class WordsControllerTest < ActionController::TestCase
   end
 
   test "should get show with existing Word" do
-    @test_word = Word.create title: "Test-Title", body: "Some Text for the Test."
+    @test_word = Word.create title: "Test-Title", body: "Some Text for the Test.", language_id: @lang.id
     get :show, {id: 'Test-Title'}
     assert_template :show
     assert_response :success
@@ -25,19 +29,20 @@ class WordsControllerTest < ActionController::TestCase
 
   test "should get new" do
     get :new
+    assert_template :new
     assert_response :success
   end
   
   test "should create word" do
     assert_difference('Word.count') do
-      post :create, word: {title: 'Test Title', body: 'Some Text for the body.'}
+      post :create, word: {title: 'Test Title', body: 'Some Text for the body.', language_id: @lang.id}
     end
     
     assert_redirected_to word_path(assigns(:word))
   end
   
   test "should update word" do
-    @test_word = Word.create title: "Test Title", body: "Test body for the Test of Update."
+    @test_word = Word.create title: "Test Title", body: "Test body for the Test of Update.", language_id: @lang.id
     
     patch :update, id: @test_word.id, word: { :title => "New Title" }
     assert_not_nil Word.find('New_Title')
@@ -46,14 +51,14 @@ class WordsControllerTest < ActionController::TestCase
   end
   
   test "should not update word with invalid data" do
-    @test_word = Word.create title: "Test Title", body: "Test body for the Test of Update."
+    @test_word = Word.create title: "Test Title", body: "Test body for the Test of Update.", language_id: @lang.id
     
     patch :update, id: @test_word.id, word: { :title => "" }
     assert_template :edit, id: @test_word.id
   end
   
   test "should destroy word" do
-    @test_word = Word.create title: "Test Title", body: "Test body for the Test of Update."
+    @test_word = Word.create title: "Test Title", body: "Test body for the Test of Update.", language_id: @lang.id
     
     assert_difference('Word.count', -1) do
       delete :destroy, id: @test_word.id
