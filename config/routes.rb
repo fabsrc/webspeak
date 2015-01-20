9de9ca5e6171798dda27ecc0857ab4cd2818c338
fbatz <fabio.batz@googlemail.com>
2015-01-20 14:37:27 +0000

    Sign up /Log in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+
+  get 'signup'      => 'users#new'
+  get 'login'       => 'sessions#new'
+  post 'login'      => 'sessions#create'
+  delete 'logout'   => 'sessions#destroy'
+
+  resources :users
+
   resources :words, :path => '' do
     collection do
       get 'autocomplete'
       get 'search'
     end
   end
+
+  root to: 'words#index'
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141218182542) do
+ActiveRecord::Schema.define(version: 20150113151755) do
 
   create_table "friendly_id_slugs", force: true do |t|
     t.string   "slug",                      null: false
@@ -25,6 +25,18 @@ ActiveRecord::Schema.define(version: 20141218182542) do
   add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "role"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "password_digest"
+    t.string   "remember_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
   create_table "words", force: true do |t|
     t.string   "title"
